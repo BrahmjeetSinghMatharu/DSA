dc43fab25d2d99902b51571d2b853820aaa7e8de
Brahmjeet Singh <brahmjeetsinghmatharu06@gmail.com>
2026-07-30 17:24:26 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (99.68%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-    void generateSubsets(int index,vector<int> &subset,vector<vector<int>> &ans,vector<int> &nums){
-        if(index == nums.size()){
-            ans.push_back(subset);
-            return;
+    void solve(int index,vector<int>& nums,vector<int>& subset,vector<vector<int>>& ans) {
+        // save the current state
+        ans.push_back(subset);
+
+        // try next choices
+        for(int i=index;i<nums.size();i++){
+            // choose
+            subset.push_back(nums[i]);
+
+            // explore
+            solve(i+1,nums,subset,ans);
+
+            // undo
+            subset.pop_back();
         }
-
-        // Include
-        subset.push_back(nums[index]);
-        generateSubsets(index+1,subset,ans,nums);
-
-        // Exclude
-        subset.pop_back();
-        generateSubsets(index+1,subset,ans,nums);
     }
 
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> subset;
-        generateSubsets(0,subset,ans,nums);
+
+        solve(0, nums, subset, ans);
+
         return ans;
     }
 };
