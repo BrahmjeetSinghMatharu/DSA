5dbf143f3c03d485c26b2080ea836a8920b8b7af
Brahmjeet Singh <brahmjeetsinghmatharu06@gmail.com>
2026-08-03 23:59:59 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (31.36%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -2,27 +2,26 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int> l2r(n,1);
-        vector<int> r2l(n,1);
+        vector<int> count(n,1);
 
         // left to right
         for(int i=1;i<n;i++){
             if(ratings[i-1] < ratings[i]){
-                l2r[i] = l2r[i-1]+1;
+                count[i] = count[i-1]+1;
             }
         }
 
         // right to left
         for(int i=n-2;i>=0;i--){
             if(ratings[i+1] < ratings[i]){
-                r2l[i] = r2l[i+1]+1;
+                count[i] = max(count[i],count[i+1]+1);
             }
         }
 
         // final answer
         int ans = 0;
         for(int i=0;i<n;i++){
-            ans += max(l2r[i],r2l[i]);
+            ans += count[i];
         }
         return ans;
     }
