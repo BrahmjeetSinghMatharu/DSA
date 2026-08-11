f9a0c16ee59db561da323dd7a58b3a7b3507e305
Brahmjeet Singh <brahmjeetsinghmatharu06@gmail.com>
2026-08-11 18:31:08 +0530

    Time: 107 ms (39.23%) | Memory: 51.3 MB (43.45%) - LeetSync

---
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,62 @@
+class node{
+    public:
+    int data;
+    int row;
+    int col;
+
+    node(int d,int i,int j){
+        data = d;
+        row = i;
+        col = j;
+    }
+};
+
+class compare{
+    public:
+    bool operator()(node* a,node* b){
+        return a->data > b->data;
+    }
+};
+
+class Solution {
+public:
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        int n = nums.size();
+
+        priority_queue<node*,vector<node*>,compare> pq;
+        int mini = INT_MAX;
+        int maxi = INT_MIN;
+
+        for(int i=0;i<n;i++){
+            mini = min(mini,nums[i][0]);
+            maxi = max(maxi,nums[i][0]);
+            pq.push(new node(nums[i][0],i,0));
+        }
+
+        int start = mini;
+        int end = maxi;
+
+        while(!pq.empty()){
+            node* temp = pq.top();
+            pq.pop();
+
+            mini = temp->data;
+
+            // Update the answer
+            if((maxi - mini) < (end-start)){
+                end = maxi;
+                start = mini;
+            }
+
+            // Insert next element
+            if(temp->col + 1 < nums[temp->row].size()){
+                maxi = max(maxi,nums[temp->row][temp->col+1]);
+                pq.push(new node(nums[temp->row][temp->col+1],temp->row,temp->col+1));
+            }
+            else{
+                break;
+            }
+        }
+        return {start,end};
+    }
+};
