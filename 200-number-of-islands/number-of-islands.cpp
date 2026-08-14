f97bdedb33b170ff06a957169a95ab0b9181e79b
Brahmjeet Singh <brahmjeetsinghmatharu06@gmail.com>
2026-08-15 00:57:36 +0530

    Time: 185 ms (5.09%) | Memory: 56.6 MB (5.04%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,47 +1,39 @@
 class Solution {
 public:
-    void bfs(int i,int j,vector<vector<bool>>& vis,vector<vector<char>>& grid){
+    void dfs(int i, int j, vector<vector<bool>>& vis,vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
 
-        queue<pair<int,int>> q;
-        q.push({i,j});
         vis[i][j] = true;
 
-        vector<vector<int>> directions = {{1,0},{0,1},{-1,0},{0,-1}};
+        vector<vector<int>> directions = {{1, 0},{0, 1},{-1, 0},{0, -1}};
 
-        while(!q.empty()){
-            auto it = q.front();
-            q.pop();
+        for (auto& dir : directions) {
+            int new_x = i + dir[0];
+            int new_y = j + dir[1];
 
-            int x = it.first;
-            int y = it.second;
+            if (new_x >= 0 && new_x < n &&
+                new_y >= 0 && new_y < m &&
+                !vis[new_x][new_y] &&
+                grid[new_x][new_y] == '1') {
 
-            for(auto& dir : directions){
-                int new_x = x + dir[0];
-                int new_y = y + dir[1];
-                
-                if(new_x>=0 && new_x<n && new_y>=0 && new_y<m && !vis[new_x][new_y] && grid[new_x][new_y] == '1'){
-                    q.push({new_x,new_y});
-                    vis[new_x][new_y] = true;
-                }
-
+                dfs(new_x, new_y, vis, grid);
             }
-
         }
     }
 
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<bool>> vis(n,vector<bool>(m,false));
+
+        vector<vector<bool>> vis(n, vector<bool>(m, false));
 
         int islands = 0;
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(!vis[i][j] && grid[i][j] == '1'){
-                    bfs(i,j,vis,grid);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!vis[i][j] && grid[i][j] == '1') {
+                    dfs(i, j, vis, grid);
                     islands++;
                 }
             }
