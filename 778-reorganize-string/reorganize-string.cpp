7f4919f1e31c2fed3c7e6f8ed55cfcf97d1dbe4f
Brahmjeet Singh <brahmjeetsinghmatharu06@gmail.com>
2026-08-14 17:00:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (29.99%) - LeetSync

---
+++ 778-reorganize-string/reorganize-string.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        int n = s.length();
+        vector<int> freq(26,0);
+
+        for(char& ch : s){
+            freq[ch-'a']++;
+
+            if(freq[ch-'a'] > (n+1)/2) return "";
+        }
+
+        priority_queue<pair<int,char>> pq;
+
+        for(int i=0;i<26;i++){
+            if(freq[i]>0){
+                pq.push({freq[i],'a'+i});
+            }
+        }
+
+        string res = "";
+
+        while(pq.size()>=2){
+            auto p1 = pq.top();
+            pq.pop();
+
+            auto p2 = pq.top();
+            pq.pop();
+
+            res.push_back(p1.second);
+            p1.first--;
+            res.push_back(p2.second);
+            p2.first--;
+
+            if(p1.first>0) pq.push(p1);
+            if(p2.first>0) pq.push(p2);
+        }
+        
+        if(!pq.empty()){
+            res.push_back(pq.top().second);
+        }
+        return res;
+    }
+};
