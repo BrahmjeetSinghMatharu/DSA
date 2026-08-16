1d3728d69861e4b1c62025b829111c8c66230ca4
Brahmjeet Singh <brahmjeetsinghmatharu06@gmail.com>
2026-08-16 13:30:19 +0530

    Time: 13 ms (23.54%) | Memory: 29.4 MB (89.38%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,47 +1,49 @@
 class Solution {
 public:
     vector<vector<int>> directions = {{1,0},{0,1},{-1,0},{0,-1}};
-        
-    void dfs(int i,int j,vector<vector<bool>>& vis,vector<vector<int>>& grid){
-        int n = grid.size();
-        int m = grid[0].size();
-
-        vis[i][j] = 1;
-
-        for(auto& dir : directions){
-            int x = i + dir[0];
-            int y = j + dir[1];
-
-            if(x>=0 && x<n && y>=0 && y<m && !vis[x][y] && grid[x][y] == 1){
-                dfs(x,y,vis,grid);
-            }
-        }
-
-    }
 
     int numEnclaves(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
 
         vector<vector<bool>> vis(n,vector<bool>(m,false));
+        queue<pair<int,int>> q;
 
         // Top & Bottom Boundary
         for(int j=0;j<m;j++){
             if(grid[0][j] == 1 && !vis[0][j]){
-                dfs(0,j,vis,grid);
+                q.push({0,j});
             }
             if(grid[n-1][j] == 1 && !vis[n-1][j]){
-                dfs(n-1,j,vis,grid);
+                q.push({n-1,j});
             }
         }
 
         // Left & Right Boundary
         for(int i=0;i<n;i++){
             if(grid[i][0] == 1 && !vis[i][0]){
-                dfs(i,0,vis,grid);
+                q.push({i,0});
             }
             if(grid[i][m-1] == 1 && !vis[i][m-1]){
-                dfs(i,m-1,vis,grid);
+                q.push({i,m-1});
+            }
+        }
+
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            vis[row][col] = true;
+
+            for(auto& dir : directions){
+                int x = row + dir[0];
+                int y = col + dir[1];
+
+                if(x>=0 && x<n && y>=0 && y<m && !vis[x][y] && grid[x][y] == 1){
+                    vis[x][y] = true;
+                    q.push({x,y});
+                }
             }
         }
 
