f16a8fa2c6394f7808710858aef7551d4939a2a3
Brahmjeet Singh <brahmjeetsinghmatharu06@gmail.com>
2026-09-01 16:08:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (54.67%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int solve(vector<vector<int>>& dp,int m,int n){
+        if(m==1 && n==1) return 1;
+        if(m==0 || n==0) return 0;
+
+        if(dp[m][n] != -1) return dp[m][n]; 
+
+        int rightWays = solve(dp,m,n-1);
+        int downWays = solve(dp,m-1,n);
+        return dp[m][n] = rightWays + downWays;
+    }
+
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        return solve(dp,m,n);
+    }
+};
